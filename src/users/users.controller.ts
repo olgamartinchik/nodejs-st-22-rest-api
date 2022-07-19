56099ce3dc1eb17521f65aa92cadbec3d6adfdbc
olgamartinchik <sintcerova@mail.ru>
2022-07-19 18:54:01 +0300

    feat: add service, entity, module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,55 +1,36 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put } from "@nestjs/common";
+import { IUser } from "./user.interface";
+import { UserService } from "./users.service";
 
-interface IUser{
-    id:string;
-    login:string;
-    password:string;
-    age:number;
-    isDeleted: boolean;
-}
+
 
 @Controller('v1')
 export class UserController{
-    private users:IUser[]=[{
-        id:'string',
-        login:'string',
-        password:'string',
-        age:0,
-        isDeleted: false
-    },
-    {
-        id:'string1',
-        login:'string',
-        password:'string',
-        age:0,
-        isDeleted: true
-    }
-]
+constructor(private  userService:UserService){}
     @Get('users')
     @HttpCode(200)
-    getAllUsers():IUser[]{
-        const users = this.users.filter(user=>!user.isDeleted)
-        return users
+    getAllUsers():IUser[]{       
+        return this.userService.getAllUsers()
     }
     @Get('users/:id')
     @HttpCode(200)
     getUserById(@Param() params):IUser{
-        const user = this.users.find(user=>user.id===params.id&&!user.isDeleted)
-        return user
+        return this.userService.getUserById(params)
     }
 
    
     @Post('users')
     postUserData(@Body('user') user:IUser):{user:IUser, message:string}{
-        this.users.push(user)
-     return {user, message:"User created"}
+     return this.userService.postUserData(user)
     }
+
     @Put('users/id')
-    updateUserData(){}
+    updateUserData(@Body('user') user:IUser):{user:IUser, message:string}{
+        return this.userService.updateUserData(user)
+    }
 
     @Delete('users/id')
-    deleteUser(@Param() params):{user:IUser, message:string}{
-        const user= this.users.find(user=>user.id===params.id?user.isDeleted=true:user.isDeleted=false)
-        return {user, message:"User deleted"}
+    deleteUser(@Param() params:IUser):{user:IUser, message:string}{        
+        return this.userService.deleteUser(params)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,43 @@
-import { Injectable } from "@nestjs/common";
+import {  Injectable} from "@nestjs/common";
+import { User } from "./user.entity";
+import { IUser } from "./user.interface";
 
 @Injectable()
 export class UserService{
+    private users:IUser[]=[{
+        id:'string',
+        login:'string',
+        password:'string',
+        age:0,
+        isDeleted: false
+    },
+    {
+        id:'string1',
+        login:'string',
+        password:'string',
+        age:0,
+        isDeleted: true
+    }
+]
 
+getAllUsers():IUser[]{
+    const users = this.users.filter(user=>!user.isDeleted)
+    return users
+}
+getUserById( params:IUser):IUser{
+    const user = this.users.find(user=>user.id===params.id&&!user.isDeleted)
+    return user
+}
+postUserData( user:IUser):{user:IUser, message:string}{
+   const newUser=new User(user.login,user.password,user.age,)
+    this.users.push(newUser)
+ return {user:newUser, message:"User created"}
+}
+updateUserData(user:IUser):{user:IUser, message:string}{
+    return {user, message:"User update"}
+}
+deleteUser( params:IUser):{user:IUser, message:string}{
+    const user= this.users.find(user=>user.id===params.id?user.isDeleted=true:user.isDeleted=false)
+    return {user, message:"User deleted"}
+}
 }
