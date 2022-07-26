c85f1015075c77f909372e87584151ee7f1089bc
olgamartinchik <sintcerova@mail.ru>
2022-07-26 16:32:09 +0300

    init: init postgresql task

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,6 +1,8 @@
 import { IUser } from './user.interface';
 import { v4 as uuidv4 } from 'uuid';
+import { Entity } from 'typeorm';
 
+@Entity('users')
 export class User implements IUser{
     
     id:string;
