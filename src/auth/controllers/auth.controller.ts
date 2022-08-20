a764b0dd16adf0a1922730f1b81243b6d9b9b678
olgamartinchik <sintcerova@mail.ru>
2022-08-20 20:19:30 +0300

    feat: add guard in the group controller

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,10 +1,12 @@
-import { BadRequestException, Body, Controller, ForbiddenException, Post, UnauthorizedException, UseFilters } from '@nestjs/common';
+import { BadRequestException, Body, Controller, ForbiddenException, Post, UnauthorizedException, UseFilters, UseGuards } from '@nestjs/common';
 import { HttpExceptionFilter } from '@src/filter/http-exception.filter';
 import { CreateUserDto } from '@src/users/dto/create-user.dto';
+import { JwtAuthGuard } from '../guard/jwt.auth.guard';
 import { AuthService } from '../services/auth.service';
 
-// @ApiTags('auth')
+
 @Controller('v1/auth')
+@UseGuards(JwtAuthGuard)
 export class AuthController {
     constructor (private authService:AuthService){}
 
